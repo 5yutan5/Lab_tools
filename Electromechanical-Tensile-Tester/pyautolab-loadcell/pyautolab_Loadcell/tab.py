031385345415393575196affbe5d8947db36d808
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-06-26 22:34:04 +0900

    Update

--- Electromechanical-Tensile-Tester/pyautolab-loadcell/pyautolab_Loadcell/tab.py
+++ Electromechanical-Tensile-Tester/pyautolab-loadcell/pyautolab_Loadcell/tab.py
@@ -13,7 +13,7 @@ class TabLoadcell(api.DeviceTab):
         super().__init__(device)
         self._ui = _TabUI()
         self._ui.setup_ui(self)
-        self._ui._set_zero_button.clicked.connect(device.fix_zero)
+        self._ui._set_zero_button.clicked.connect(device.fix_zero)  # type: ignore
 
     def get_parameters(self) -> dict[str, str]:
         """Override Device class."""
